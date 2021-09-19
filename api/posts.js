a4a5c50769b5c86c84a846f6a9b89340f2191a1f
Brandon Fillpot <brandonfillpot@gmail.com>
2021-09-19 14:43:43 -0700

    writing post done

--- api/posts.js
+++ api/posts.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const postsRouter = express.Router();
 const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
   next();
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
 });
 
 postsRouter.get('/', async (req, res) => {
