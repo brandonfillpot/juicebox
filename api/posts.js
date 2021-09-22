6331432f00bbed1b18b79cdbe0578187af731112
Brandon Fillpot <brandonfillpot@gmail.com>
2021-09-22 11:37:59 -0700

    almost done.. gosh darnit

--- api/posts.js
+++ api/posts.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { 
+  getAllPosts,
+  createPost
+ } = require('../db');
 const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+  
+    
+
+    const post = await createPost(postData)
+
+    if (post) {
+      res.send({ post });
+    } else {
+      next({ 
+        name: 'Error Posting', 
+        message: 'try something completely different'
+      });
+    }
+    
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
   next();
-});
-
-postsRouter.post('/', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction' });
 });
 
 postsRouter.get('/', async (req, res) => {
